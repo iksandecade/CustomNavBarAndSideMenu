6cfe36ceaeb4105bb2e606e15bfd60245f505b72
Ihksan Sukmawan <ihksan.sukmawan@mncgroup.com>
2023-07-06 15:42:45 +0700

    Initial Commit

---
+++ CustomNavBarAndSideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomNavBarAndSideMenu
+//
+//  Created by MCOMM00042 on 06/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
